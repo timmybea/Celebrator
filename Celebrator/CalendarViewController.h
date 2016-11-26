773601c3e3fc5e4e62816a4965e888f37713e2e2
Tim Beals <tim.beals@gmail.com>
2016-11-26 14:48:22 -0800

    FSCalendar framework installed

---
+++ Celebrator/CalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarViewController.h
+//  Celebrator
+//
+//  Created by Tim Beals on 2016-11-26.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UIViewController
+
+@end
---
+++ Celebrator/CalendarViewController.m
@@ -0,0 +1,24 @@
+//
+//  CalendarViewController.m
+//  Celebrator
+//
+//  Created by Tim Beals on 2016-11-26.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+#import "FSCalendar.h"
+
+@interface CalendarViewController () <FSCalendarDelegate, FSCalendarDataSource>
+
+@end
+
+@implementation CalendarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
